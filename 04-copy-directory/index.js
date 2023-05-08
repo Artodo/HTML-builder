0c88a62a05f4257fb61b29940911a0e1966bd97f
Artur Dyshynski <tavebran@gmail.com>
2023-05-08 13:02:30 +0300

    feat: add solutions for 4 tack

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs');
+
+const DESTINATION_FOLDER = './04-copy-directory/files-copy';
+const SOURCE_FOLDER = './04-copy-directory/files';
+
+fs.mkdir(DESTINATION_FOLDER, { recursive: true }, err => {
+  if (err) throw err;
+  console.log('Папка успешно создана');
+
+
+  fs.readdir(SOURCE_FOLDER, (err, files) => {
+    if (err) throw err;
+    console.log('В папке находятся файлы:' + files);
+
+    files.forEach(file => {
+      fs.copyFile(`${SOURCE_FOLDER}/${file}`, `${DESTINATION_FOLDER}/${file}`, err => {
+        if (err) throw err;
+      
+      });
+    });
+  });
+
+
+});
+
 
