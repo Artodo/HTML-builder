1a5b1ba709f85b691d4dcf0bf146ae9eff3d4043
Artur Dyshynski <tavebran@gmail.com>
2023-05-08 20:40:28 +0300

    refactor: move strings to consts

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,13 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
+const SOURCE_FOLDER = './05-merge-styles/styles';
+const DESTINATION_FILE = './05-merge-styles/project-dist/bundle.css';
+
 const mergeStyles = () => {
-  fs.open('./05-merge-styles/project-dist/bundle.css', 'w', (err) => {
+  fs.open(DESTINATION_FILE, 'w', (err) => {
     if (err) throw err;
     console.log('File created');
   });
 
-  fs.readdir('./05-merge-styles/styles', (err, files) => {
+  fs.readdir(SOURCE_FOLDER, (err, files) => {
     if (err) throw err;
     console.log('В папке находятся файлы:' + files);
 
@@ -18,11 +21,10 @@ const mergeStyles = () => {
       if (extName === 'css') {
         console.log('Это файл формата ' + extName);
 
-        fs.readFile('./05-merge-styles/styles/' + file, 'utf8', (err, data) => {
+        fs.readFile(SOURCE_FOLDER + '/' + file, 'utf8', (err, data) => {
           if (err) throw err;
-          // console.log(data);
 
-          fs.appendFile('./05-merge-styles/project-dist/bundle.css', data, (err) => {
+          fs.appendFile(DESTINATION_FILE, data, (err) => {
             if (err) throw err;
             console.log('Данные добавлены !');
           });
