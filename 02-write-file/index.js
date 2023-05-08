9b771d9a78caf25265820555a08a678fca17301e
Artur Dyshynski <tavebran@gmail.com>
2023-05-08 20:16:19 +0300

    feat: add solutions for 2 tack

---
+++ 02-write-file/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('node:process');
+
+const DESTINATION_FILE = path.join('./02-write-file/text.txt');
+
+const stream = fs.createWriteStream(DESTINATION_FILE, 'utf-8');
+console.log('Введите текст:');
+
+process.stdin.on('data', data => {
+  if (data.toString().trim() === 'exit') {
+    console.log('Выход');
+    process.exit();
+  }
+  else {
+    stream.write(data, 'utf-8');
+  }
+});
+process.addListener('SIGINT', ()=> {
+  console.log('Выход');
+  process.exit();
+});
